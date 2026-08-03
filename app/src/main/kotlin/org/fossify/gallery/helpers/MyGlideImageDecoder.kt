9ca0a88096cc6974b17ca891cd5e5bb2b8a8a2a6
wrzlpete <wiz.mage@gmx.net>
2026-08-03 18:39:19 +0200

    refactor: replace Glide-based image decoder with BitmapFactory for SubsamplingScaleImageView to fix zoom reset after initial load
    - Remove Glide dependency from MyGlideImageDecoder, use BitmapFactory.decodeStream() instead
    - Remove rotation and signature parameters from MyGlideImageDecoder constructor
    - Delete RotateTransformation class (no longer needed)
    - Fix URI encoding for special characters (%, #) in decode()
    - Add explicit ARGB_8888 config and error handling for null bitmaps
    - Update detekt baseline to remove RotateTransformation entry

--- app/src/main/kotlin/org/fossify/gallery/helpers/MyGlideImageDecoder.kt
+++ app/src/main/kotlin/org/fossify/gallery/helpers/MyGlideImageDecoder.kt
@@ -2,29 +2,22 @@ package org.fossify.gallery.helpers
 
 import android.content.Context
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DecodeFormat
-import com.bumptech.glide.request.RequestOptions
-import com.bumptech.glide.request.target.Target
-import com.bumptech.glide.signature.ObjectKey
 import com.davemorrissey.labs.subscaleview.ImageDecoder
 
-class MyGlideImageDecoder(val degrees: Int, val signature: ObjectKey) : ImageDecoder {
+class MyGlideImageDecoder : ImageDecoder {
 
     override fun decode(context: Context, uri: Uri): Bitmap {
-        val options = RequestOptions()
-            .format(DecodeFormat.PREFER_ARGB_8888)
-            .signature(signature)
-            .fitCenter()
-
-        val builder = Glide.with(context)
-            .asBitmap()
-            .load(uri.toString().substringAfter("file://"))
-            .apply(options)
-            .transform(RotateTransformation(-degrees))
-            .submit(Target.SIZE_ORIGINAL, Target.SIZE_ORIGINAL)
-
-        return builder.get()
+        val newUri = Uri.parse(uri.toString().replace("%", "%25").replace("#", "%23"))
+        val inputStream = context.contentResolver.openInputStream(newUri)
+            ?: throw RuntimeException("Cannot open input stream for $uri")
+        inputStream.use { stream ->
+            val options = BitmapFactory.Options().apply {
+                inPreferredConfig = Bitmap.Config.ARGB_8888
+            }
+            return BitmapFactory.decodeStream(stream, null, options)
+                ?: throw RuntimeException("BitmapFactory returned null bitmap - image format may not be supported")
+        }
     }
 }
